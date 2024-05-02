f34be921d63b319c5891210fd90506d0b48f9d80
Eduardo J. Guerra <edujguerra@gmail.com>
2024-05-01 21:26:42 -0300

    first commit

---
+++ src/test/java/br/com/edujguerra/livespringcloudconfigserver/LiveSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edujguerra.livespringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveSpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
